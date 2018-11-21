9101613ea61e684b6f9735126e8f3dfb0ae6f825
F-DALLA-VALLE <dallavalle.fabien@gmail.com>
2018-11-21 14:37:54 -0600

    Tests udpated.

--- test/test.js
+++ test/test.js
@@ -1,59 +1,42 @@
 const assert = require('assert');
-const t = require('../q-cpu-usage');
+const t = require('../q-ram-usage');
 
 const black = '#000000';
 
-describe('CpuUsage', function () {
-  let app = new t.CpuUsage();
+describe('RamUsage', function () {
+  let app = new t.RamUsage();
 
-  it('#getCpuUsage', function () {
-    app.getCpuUsage().then(percent => {
-      console.log("CPU Usage is: " + percent);
+  it('#getRamUsage', function () {
+    app.getRamUsage().then(percent => {
+      console.log("RAM Usage is: " + percent);
       assert.ok(percent);
     })
   });
 
-  it('#getColor', function() {
-    assert.equal(black, app.getColor(0, 0));
-    assert.notEqual(black, app.getColor(0, 1));
 
-    assert.equal(black, app.getColor(9, 9));
-    assert.notEqual(black, app.getColor(9, 10));
-  })
-
-  it('#generatePoints', function () {
-    const testLights = function (percent) {
-      const points = app.generatePoints(percent);
-      for (let i = 0; i < points.length; i += 1) {
-        if (points[i].color === black) {
-          return i;
-        }
-      }
-
-      return points.length;
+  it('#generateColor', function () {
+    const testLight = function (percent) {
+      const points = app.generateColor(percent);
+      return points[0].color;
     }
 
-    const assertLights = function (percent, expected) {
-      const actual = testLights(percent);
-      const message = `For CPU usage ${percent}, I have ${actual} lights `
+    const assertLight = function (percent, expected) {
+      const actual = testLight(percent);
+      const message = `For RAM usage ${percent}, I have a ${actual} light `
         + `(expected ${expected})`;  
       assert(expected === actual, message);
     }
 
-    assertLights(0.04, 0);
-    assertLights(0.05, 1);
-    assertLights(0.1, 1);
-    assertLights(0.2, 2);
-    assertLights(0.3, 3);
-    assertLights(0.4, 4);
-    assertLights(0.5, 5);
-    assertLights(0.6, 6);
-    assertLights(0.7, 7);
-    assertLights(0.8, 8);
-    assertLights(0.9, 9);
-    assertLights(0.95, 10);
-    assertLights(1.0, 10);
-    assertLights(1.1, 10);
+    assertLight(30, '#00FF00');
+    assertLight(50, '#00FF00');
+    assertLight(60, '#FFFF00');
+    assertLight(60, '#FFFF00');
+    assertLight(70, '#FFFF00');
+    assertLight(80, '#FF0000');
+    assertLight(90, '#FF0000');
+    assertLight(100, '#FF0000');
+
+
   });
 
   it('#run()', function () {
